8a26915b6ea594c62fd84c42ff9b615564827b0e
Marat Radchenko <marat@slonopotamus.org>
2014-03-27 18:30:31 +0400

    Build zlib ourself

--- build.sh
+++ build.sh
@@ -13,10 +13,17 @@ export VC_ARCH="${1:-x86}"
 export ARCH="${VC_ARCH/x86_amd64/amd64}"
 
 LIBS="${DIR}/lib"
-export ZLIB_PATH="${LIBS}/zlib-1.2.8.1/${ARCH}"
+export ZLIB_PATH="${DIR}/zlib"
 export OPENSSLDIR="${LIBS}/openssl-1.0.1.24/${ARCH}"
 export CURLDIR="${LIBS}/curl-7.30.0.2/${ARCH}"
 export DESTDIR="${DIR}/stage-${ARCH}"
 
-cmd /c "helper.bat make -C '${DIR}/git' MSVC=1 INLINE=__inline prefix=/ NO_TCLTK=1 V=1 NO_PERL=1 install"
-find "${OPENSSLDIR}" "${ZLIB_PATH}" "${CURLDIR}" -name \*.dll -print0 | xargs -0 -I{} cp "{}" "${DESTDIR}/bin"
+pushd "${ZLIB_PATH}"
+cmd /c "${DIR}/helper.bat nmake /f win32/Makefile.msc"
+popd
+
+pushd "${DIR}/git"
+cmd /c "${DIR}/helper.bat make MSVC=1 INLINE=__inline prefix=/ NO_TCLTK=1 V=1 NO_PERL=1 install"
+popd
+
+find "${OPENSSLDIR}" "${CURLDIR}" -name \*.dll -print0 | xargs -0 -I{} cp "{}" "${DESTDIR}/bin"
