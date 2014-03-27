530fade8a42d37ad77240f1ea8527bfba9530ab9
Marat Radchenko <marat@slonopotamus.org>
2014-03-27 18:30:39 +0400

    Build shared zlib

--- build.sh
+++ build.sh
@@ -19,11 +19,12 @@ export CURLDIR="${LIBS}/curl-7.30.0.2/${ARCH}"
 export DESTDIR="${DIR}/stage-${ARCH}"
 
 pushd "${ZLIB_PATH}"
-cmd /c "${DIR}/helper.bat nmake /f win32/Makefile.msc clean all"
+cmd /c "${DIR}/helper.bat nmake /f win32/Makefile.msc clean zlib1.dll"
+ln -s zdll.lib zlib.lib
 popd
 
 pushd "${DIR}/git"
 cmd /c "${DIR}/helper.bat make MSVC=1 INLINE=__inline prefix=/ NO_TCLTK=1 V=1 NO_PERL=1 install"
 popd
 
-find "${OPENSSLDIR}" "${CURLDIR}" -name \*.dll -print0 | xargs -0 -I{} cp "{}" "${DESTDIR}/bin"
+find "${ZLIB_PATH}" "${OPENSSLDIR}" "${CURLDIR}" -name \*.dll -print0 | xargs -0 -I{} cp "{}" "${DESTDIR}/bin"
