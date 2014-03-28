b6b3c13f5bd2946c400ba60e57ce814880682d2a
Marat Radchenko <marat@slonopotamus.org>
2014-03-28 23:46:45 +0400

    Support different versions of Visual Studio

--- helper.bat
+++ helper.bat
@@ -1,7 +1,26 @@
 @echo off
 
-call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" %VC_ARCH%
-if %errorlevel% neq 0 exit /b %errorlevel%
+if not "%VS120COMNTOOLS%" == "" (
+	call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" %VC_ARCH%
+	if %errorlevel% neq 0 exit %errorlevel%
+	goto run
+)
 
+if not "%VS110COMNTOOLS%" == "" (
+	call "%VS110COMNTOOLS%\..\..\VC\vcvarsall.bat" %VC_ARCH%
+	if %errorlevel% neq 0 exit %errorlevel%
+	goto :run
+)
+
+if not "%VS100COMNTOOLS%" == "" (
+	call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" %VC_ARCH%
+	if %errorlevel% neq 0 exit %errorlevel%
+	goto :run
+)
+
+echo ERROR: No Visual Studio found
+exit 1
+
+:run
 %*
 exit %errorlevel%
