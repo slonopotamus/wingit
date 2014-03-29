14079af173cca91abcd0256d193fa9ee9e449eb5
Marat Radchenko <marat@slonopotamus.org>
2014-03-29 16:49:51 +0400

    Fixed mingw messing with /c flag

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ export DESTDIR="${DIR}/stage-${ARCH}"
 
 export ZLIB_PATH="${DIR}/zlib"
 pushd "${ZLIB_PATH}"
-cmd /c "${DIR}/helper.bat nmake /f win32/Makefile.msc clean zlib1.dll"
+cmd.exe /c "${DIR}/helper.bat nmake /f win32/Makefile.msc clean zlib1.dll"
 # For git
 ln -s zdll.lib zlib.lib
 popd
@@ -27,21 +27,21 @@ export OPENSSLDIR="${DIR}/openssl"
 export OPENSSL_PATH="${OPENSSLDIR}"
 pushd "${OPENSSLDIR}"
 if [ "$ARCH" = "amd64" ]; then
-	cmd /c "${DIR}/helper.bat perl Configure VC-WIN64A no-asm"
+	cmd.exe /c "${DIR}/helper.bat perl Configure VC-WIN64A no-asm"
 else
-	cmd /c "${DIR}/helper.bat perl Configure VC-WIN32 no-asm"
+	cmd.exe /c "${DIR}/helper.bat perl Configure VC-WIN32 no-asm"
 fi
 # HACK, EOL troubles
 sed -i 's/\\/\//g' ms/do_win64a.bat ms/do_ms.bat
 # HACK, see http://stackoverflow.com/questions/7680189/openssl-cant-build-in-vc-2010
 sed -i 's/chop;$/chop;s\/\\r$\/\/;/' util/mk1mf.pl
 if [ "$ARCH" = "amd64" ]; then
-	cmd /c "${DIR}/helper.bat cmd /c ms\\do_win64a.bat"
+	cmd.exe /c "${DIR}/helper.bat cmd.exe /c ms\\do_win64a.bat"
 else
-	cmd /c "${DIR}/helper.bat cmd /c ms\\do_ms.bat"
+	cmd.exe /c "${DIR}/helper.bat cmd.exe /c ms\\do_ms.bat"
 fi
-cmd /c "${DIR}/helper.bat nmake /f ms/ntdll.mak clean" || true
-cmd /c "${DIR}/helper.bat nmake /f ms/ntdll.mak"
+cmd.exe /c "${DIR}/helper.bat nmake /f ms/ntdll.mak clean" || true
+cmd.exe /c "${DIR}/helper.bat nmake /f ms/ntdll.mak"
 # For git
 cp -R inc32/* include
 cp out32dll/ssleay32.lib out32dll/libeay32.lib .
@@ -49,18 +49,18 @@ popd
 
 export CURLDIR="${DIR}/curl"
 pushd "${CURLDIR}"
-cmd /c "${DIR}/helper.bat buildconf.bat"
+cmd.exe /c "${DIR}/helper.bat buildconf.bat"
 popd
 pushd "${CURLDIR}/lib"
-cmd /c "${DIR}/helper.bat nmake /f Makefile.vc6 clean"
-cmd /c "${DIR}/helper.bat nmake /f Makefile.vc6 MACHINE=${ARCH/amd64/x64} CFG=release-dll-ssl-dll-zlib-dll all"
+cmd.exe /c "${DIR}/helper.bat nmake /f Makefile.vc6 clean"
+cmd.exe /c "${DIR}/helper.bat nmake /f Makefile.vc6 MACHINE=${ARCH/amd64/x64} CFG=release-dll-ssl-dll-zlib-dll all"
 # For git
 cp libcurl_imp.lib "${CURLDIR}/libcurl.lib"
 cp libcurl_imp.lib "${CURLDIR}/libcurl.lib"
 popd
 
 pushd "${DIR}/git"
-cmd /c "${DIR}/helper.bat make MSVC=1 prefix=/ NO_TCLTK=1 NO_PERL=1 CFLAGS=-Zi LDFLAGS=-debug clean install"
+cmd.exe /c "${DIR}/helper.bat make MSVC=1 prefix=/ NO_TCLTK=1 NO_PERL=1 CFLAGS=-Zi LDFLAGS=-debug clean install"
 popd
 
 cp "${ZLIB_PATH}/zlib1.dll" "${CURLDIR}/lib/libcurl.dll" "${OPENSSLDIR}/out32dll/libeay32.dll" "${OPENSSLDIR}/out32dll/ssleay32.dll" "${DESTDIR}/bin"
