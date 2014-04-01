5e45692333f571afaf8035831495ef4f49cbd652
Marat Radchenko <marat@slonopotamus.org>
2014-04-01 12:14:44 +0400

    Bundle vcredist in MSI.
    This fixes #8

--- helper.bat
+++ helper.bat
@@ -1,26 +1,7 @@
 @echo off
 
-if not "%VS120COMNTOOLS%" == "" (
-	call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" %VC_ARCH%
-	if %errorlevel% neq 0 exit %errorlevel%
-	goto run
-)
+call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" %VC_ARCH%
+if %errorlevel% neq 0 exit %errorlevel% exit %errorlevel%
 
-if not "%VS110COMNTOOLS%" == "" (
-	call "%VS110COMNTOOLS%\..\..\VC\vcvarsall.bat" %VC_ARCH%
-	if %errorlevel% neq 0 exit %errorlevel%
-	goto :run
-)
-
-if not "%VS100COMNTOOLS%" == "" (
-	call "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" %VC_ARCH%
-	if %errorlevel% neq 0 exit %errorlevel%
-	goto :run
-)
-
-echo ERROR: No Visual Studio found
-exit 1
-
-:run
 %*
 exit %errorlevel%
