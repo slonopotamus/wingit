8276623c7eabdca80d63b7b7628bffe2e138f8a0
Marat Radchenko <marat@slonopotamus.org>
2014-04-20 11:11:13 +0400

    helper.bat: add verbose error reporting
    Now helper.bat verbosely describes why it fails.
    This commit should fix #18.

--- helper.bat
+++ helper.bat
@@ -1,5 +1,13 @@
 @echo off
 
+if "%VS120COMNTOOLS%" == "" (
+	echo VS120COMNTOOLS environment variable is not set
+	echo This most likely means that MSVC is not installed
+	echo Also, restart of msys or even logout/login cycle
+	echo may be required after installing MSVC
+	exit 1
+)
+
 call "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" %VC_ARCH%
 if %errorlevel% neq 0 exit %errorlevel%
 
